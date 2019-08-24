bbc9d21453d5fd47d67e00cfa3fe75b7280b847c
Nie Xiaojun <nxjniexiao@163.com>
2019-08-24 19:38:03 +0800

    angular-js=nav-tree 新增单选功能

--- 29-angular-jstree-nav-tree/js-tree-directive.js
+++ 29-angular-jstree-nav-tree/js-tree-directive.js
@@ -4,7 +4,8 @@
  * @param {Array} selectedArr - 已选项数组。
  * @param {Function} onChangeFn - 已选项发生改变时的回调函数，第一参数为当前已选项数组。
  * @param {Object} [config] - 配置对象。
- * @param {boolean} [config.isSearchAvailable] - 搜索框是否可用，默认为 false。
+ * @param {boolean} [config.hasCheckbox] - 是否有选框，默认为 false。
+ * @param {boolean} [config.hasSearchBox] - 是否有搜索框，默认为 false。
  * */
 angular.module('myApp')
   .directive('jsTree', ['$timeout', function ($timeout) {
@@ -24,6 +25,7 @@ angular.module('myApp')
         scope.noSearchResults = false;
         $timeout(function () {
           jsTreeDom = $('#' + scope._id);
+          var opts = initOpts();
           jsTreeDom
             .on('open_node.jstree', function (event, data) {
               console.log('open_node');
@@ -34,17 +36,23 @@ angular.module('myApp')
             .on('ready.jstree', function (event, data) {
               console.log('ready.jstree');
             })
-            .on('select_node.jstree', function(event, data) {
+            .on('check_node', function (event, data) {
+              console.log('check_node.jstree'); // works when tie_selection is false
+            })
+            .on('uncheck_node', function (event, data) {
+              console.log('uncheck_node'); // works when tie_selection is false
+            })
+            .on('select_node.jstree', function (event, data) {
               console.log('select_node');
               // var selectedArr = data.instance.get_checked(true);
               // console.log(selectedArr);
             })
-            .on('deselect_node.jstree', function(event, data) {
+            .on('deselect_node.jstree', function (event, data) {
               console.log('deselect_node');
               // var selectedArr = data.instance.get_checked(true);
               // console.log(selectedArr);
             })
-            .on('deselect_all.jstree', function(event, data) {
+            .on('deselect_all.jstree', function (event, data) {
               console.log('deselect_all');
               // var selectedArr = data.instance.get_checked(true);
               // console.log(selectedArr);
@@ -52,13 +60,21 @@ angular.module('myApp')
             .on('changed.jstree', function (event, data) {
               console.log('changed');
               var arr = [];
-              var selectedArr = data.instance.get_checked(true);
-              selectedArr.forEach(function (item) {
-                arr.push(item.original);
-              });
+              if (scope.config.hasCheckbox) {
+                // 多选框情况
+                var selectedArr = data.instance.get_checked(true);
+                selectedArr.forEach(function (item) {
+                  arr.push(item.original);
+                });
+              } else {
+                // 取消选择时，data.selected 为 []
+                if (data.selected.length) {
+                  arr.push(data.node.original);
+                }
+              }
               scope.selectedArr = arr;
               scope.$apply();
-              scope.onChangeFn({selectedArr: arr});
+              scope.onChangeFn({ selectedArr: arr });
             })
             .on('search.jstree', function (event, data) {
               scope.noSearchResults = !data.res.length;
@@ -66,35 +82,51 @@ angular.module('myApp')
             .on('clear_search.jstree', function (event, data) {
               scope.noSearchResults = false;
             })
-            .jstree({
-              core: {
-                data: scope.treeData,
-              },
-              plugins: ['checkbox', 'conditionalselect', 'search'],
-              checkbox: {
-                three_state: true,
-                cascade: 'down+undetermined'
-              },
-              search: {
-                show_only_matches: true,
-                show_only_matches_children: true
-              },
-              conditionalselect: function (node, event) {
-                isExternalChange = false; //非外部改变已选项
-                var className = event.target.getAttribute('class');
-                var isCheckBox = /checkbox/.test(className);
-                if (isCheckBox) {
-                  // 'down': 勾选时，其子节点也会被勾选上
-                  jsTreeInstance.settings.checkbox.cascade = 'down+undetermined';
-                } else {
-                  // '': 勾选时，其子节点不会被勾选上
-                  jsTreeInstance.settings.checkbox.cascade = 'undetermined';
-                }
-                return true;
-              }
-            });
+            .jstree(opts);
           jsTreeInstance = jsTreeDom.jstree(true); // 获取 jsTree 的实例
         });
+
+        // 初始化 options
+        function initOpts() {
+          var config = scope.config;
+          var opts = {
+            core: {
+              data: scope.treeData,
+            },
+            plugins: ['conditionalselect'],
+            conditionalselect: function (node, event) {
+              isExternalChange = false; //非外部改变已选项
+              var className = event.target.getAttribute('class');
+              var isCheckBox = /checkbox/.test(className);
+              if (config.hasCheckbox && isCheckBox) {
+                // 'down': 勾选时，其子节点也会被勾选上
+                jsTreeInstance.settings.checkbox.cascade = 'down+undetermined';
+              } else {
+                // '': 勾选时，其子节点不会被勾选上
+                jsTreeInstance.settings.checkbox.cascade = 'undetermined';
+              }
+              return true;
+            }
+          };
+          // 如果有多选框
+          if (config.hasCheckbox) {
+            opts.plugins.push('checkbox');
+            opts.checkbox = {
+              three_state: true,
+              whole_node: false,
+              cascade: 'down+undetermined'
+            };
+          }
+          // 如果有搜索框
+          if (config.hasSearchBox) {
+            opts.plugins.push('search');
+            opts.search = {
+              show_only_matches: true,
+              show_only_matches_children: true
+            }
+          }
+          return opts;
+        }
 
         // 刷新 jstree 中的已选项
         function reselect(jstreeInstance, selectedArr) {
@@ -124,7 +156,7 @@ angular.module('myApp')
         }, true);
 
         // 监听搜索框
-        if (scope.config.isSearchAvailable) {
+        if (scope.config.hasSearchBox) {
           scope.$watch('keywords', function () {
             $timeout.cancel(timerForSearch);
             timerForSearch = $timeout(function () {
